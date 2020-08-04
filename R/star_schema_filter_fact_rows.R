163426405167dcfdef9f0fae55fd6787f74492f5
Jose Samos <jsamos@ugr.es>
2020-08-04 18:49:11 +0200

    Select fact rows to delete them or work with a subset of the star schema.

---
+++ R/star_schema_filter_fact_rows.R
@@ -0,0 +1,47 @@
+
+#' Filter fact rows in a star schema
+#'
+#' Filter fact rows based on dimension conditions. Dimensions remain unchanged.
+#'
+#' Filtered rows can be deleted using the `incremental_refresh_star_schema`
+#' function.
+#'
+#' @param st A `star_schema` object.
+#' @param name A string, name of the dimension.
+#' @param ... Conditions, defined in exactly the same way as in `dplyr::filter`.
+#'
+#' @return A `star_schema` object.
+#'
+#' @family incremental refresh functions
+#' @seealso
+#'
+#' @examples
+#' library(tidyr)
+#'
+#' st <- st_mrs_age %>%
+#'   filter_fact_rows(name = "when", week <= "03") %>%
+#'   filter_fact_rows(name = "where", city == "Bridgeport")
+#'
+#' st2 <- st_mrs_age %>%
+#'   incremental_refresh_star_schema(st, existing = "delete")
+#'
+#' @export
+filter_fact_rows <- function(st,
+                        name = NULL,
+                        ...) {
+  UseMethod("filter_fact_rows")
+}
+
+
+#' @rdname filter_fact_rows
+#' @export
+filter_fact_rows.star_schema <- function(st,
+                                    name = NULL,
+                                    ...) {
+  stopifnot(!is.null(name))
+  stopifnot(name %in% get_dimension_names(st))
+  dimension <- get_dimension(st, name)
+  key <- dplyr::filter(tibble::as_tibble(dimension), ...)[[1]]
+  st$fact[[1]] <- st$fact[[1]][st$fact[[1]][[sprintf("%s_key", name)]] %in% key, ]
+  st
+}
---
+++ tests/testthat/test-filter_fact_rows.R
@@ -0,0 +1,31 @@
+context("test filter_fact_rows")
+
+test_that("filter_fact_rows works", {
+  st <- st_mrs_age_test %>%
+    filter_fact_rows(name = "when", week <= "01") %>%
+    filter_fact_rows(name = "where", city == "Bridgeport")
+
+  expect_equal(
+    st$fact$mrs_age,
+    structure(
+      list(
+        when_available_key = c(3L, 3L, 3L),
+        when_key = c(1L,
+                     1L, 1L),
+        where_key = c(2L, 2L, 2L),
+        who_key = c(1L, 4L, 5L),
+        deaths = c(3L, 16L, 27L),
+        nrow_agg = c(1L, 1L, 1L)
+      ),
+      row.names = c(NA,-3L),
+      class = c("tbl_df", "tbl", "data.frame", "fact_table"),
+      name = "mrs_age",
+      foreign_keys = c("when_key",
+                       "when_available_key", "where_key", "who_key"),
+      measures = c("deaths",
+                   "nrow_agg"),
+      agg_functions = c("SUM", "SUM"),
+      nrow_agg = "nrow_agg"
+    )
+  )
+})
